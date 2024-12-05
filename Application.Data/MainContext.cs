76999c53908efa1ed2bed27023cf7c9f3d2020d3
Markiesch <marksmeel@gmail.com>
2024-12-05 11:29:00 +0100

    Packages en reference toegevoegd

--- Application.Data/MainContext.cs
+++ Application.Data/MainContext.cs
@@ -3,65 +3,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Application.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+public class MainContext(DbContextOptions<MainContext> options) : DbContext(options)
 {
     public DbSet<Animal> Animals { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Booking> Bookings { get; set; }
     public DbSet<BookingDetail> BookingDetails { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        // Beestje Configuratie
-        modelBuilder.Entity<Animal>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
-            entity.Property(e => e.Type).IsRequired().HasMaxLength(50);
-            entity.Property(e => e.Image).IsRequired().HasMaxLength(200);
-            entity.Property(e => e.Type).HasConversion<string>().IsRequired();
-        });
+        base.OnModelCreating(builder);
+        
+        CreateRelations(builder);
+        SeedData(builder);
+    }
 
-        // Klant Configuratie
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.FirstName).IsRequired().HasMaxLength(255);
-            entity.Property(e => e.Infix).IsRequired().HasMaxLength(5);
-            entity.Property(e => e.LastName).IsRequired().HasMaxLength(255);
-            entity.Property(e => e.Address).IsRequired().HasMaxLength(255);
-            entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
-            entity.Property(e => e.PhoneNumber).IsRequired().HasMaxLength(15);
-            entity.Property(e => e.CustomerCardType).HasConversion<string>().IsRequired();
-        });
-
-        // Boeking Configuratie
-        modelBuilder.Entity<Booking>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.HasOne(e => e.Klant)
-                  .WithMany(k => k.Bookings)
-                  .HasForeignKey(e => e.KlantId)
-                  .OnDelete(DeleteBehavior.Cascade);
-        });
-
-        // BoekingDetail Configuratie
-        modelBuilder.Entity<BookingDetail>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.HasOne(e => e.Booking)
-                  .WithMany(b => b.BookingDetails)
-                  .HasForeignKey(e => e.BookingId)
-                  .OnDelete(DeleteBehavior.Cascade);
-
-            entity.HasOne(e => e.Animal)
-                  .WithMany(b => b.BookingDetails)
-                  .HasForeignKey(e => e.AnimalId)
-                  .OnDelete(DeleteBehavior.Restrict);
-        });
-
+    private static void SeedData(ModelBuilder builder)
+    {
         // Seed Data
-        modelBuilder.Entity<Animal>().HasData(
+        builder.Entity<Animal>().HasData(
             // Jungle
             new Animal { Id = 1, Name = "Aap", Type = AnimalType.Jungle, Price = 25.0m, Image = "/images/aap.jpg" },
             new Animal { Id = 2, Name = "Olifant", Type = AnimalType.Jungle, Price = 100.0m, Image = "/images/olifant.jpg" },
@@ -89,21 +49,72 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
             new Animal { Id = 16, Name = "Unicorn", Type = AnimalType.Vip, Price = 1000.0m, Image = "/images/unicorn.jpg" }
         );
 
-        modelBuilder.Entity<User>().HasData(
+        builder.Entity<User>().HasData(
             new User { Id = 1, FirstName = "Jan", Infix = null, LastName = "Jansen", Address = "Straat 1, 1234 AB", Email = "jan@example.com", PhoneNumber = "0612345678", CustomerCardType = null },
             new User { Id = 2, FirstName = "Piet", Infix = null, LastName = "Pietersen", Address = "Straat 2, 1234 CD", Email = "pite@example.com", PhoneNumber = "0612345678", CustomerCardType = CustomerCardType.Silver },
             new User { Id = 3, FirstName = "Karin", Infix = null, LastName = "Klaassen", Address = "Straat 3, 1234 EF", Email = "karin@example.com", PhoneNumber = "0612345678", CustomerCardType = CustomerCardType.Gold },
             new User { Id = 4, FirstName = "Sophie", Infix = "de", LastName = "Groot", Address = "Straat 4, 1234 GH", Email = "sophie@example.com", PhoneNumber = "0612345678", CustomerCardType = CustomerCardType.Platinum }
         );
 
-        modelBuilder.Entity<Booking>().HasData(
+        builder.Entity<Booking>().HasData(
             new Booking { Id = 1, Datum = new DateTime(2024, 12, 25), Bevestigd = false, Totaalprijs = 150.0m, KlantId = 1 }
         );
 
-        modelBuilder.Entity<BookingDetail>().HasData(
+        builder.Entity<BookingDetail>().HasData(
             new BookingDetail { Id = 1, BookingId = 1, AnimalId = 1 },
             new BookingDetail { Id = 2, BookingId = 1, AnimalId = 3 },
             new BookingDetail { Id = 2, BookingId = 1, AnimalId = 7 }
         );
+    }
+
+    private static void CreateRelations(ModelBuilder builder)
+    {
+        // Beestje Configuratie
+        builder.Entity<Animal>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.Type).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.Image).IsRequired().HasMaxLength(200);
+            entity.Property(e => e.Type).HasConversion<string>().IsRequired();
+        });
+
+        // Klant Configuratie
+        builder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.FirstName).IsRequired().HasMaxLength(255);
+            entity.Property(e => e.Infix).IsRequired().HasMaxLength(5);
+            entity.Property(e => e.LastName).IsRequired().HasMaxLength(255);
+            entity.Property(e => e.Address).IsRequired().HasMaxLength(255);
+            entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.PhoneNumber).IsRequired().HasMaxLength(15);
+            entity.Property(e => e.CustomerCardType).HasConversion<string>().IsRequired();
+        });
+
+        // Boeking Configuratie
+        builder.Entity<Booking>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.HasOne(e => e.Klant)
+                .WithMany(k => k.Bookings)
+                .HasForeignKey(e => e.KlantId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        // BoekingDetail Configuratie
+        builder.Entity<BookingDetail>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.HasOne(e => e.Booking)
+                .WithMany(b => b.BookingDetails)
+                .HasForeignKey(e => e.BookingId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            entity.HasOne(e => e.Animal)
+                .WithMany(b => b.BookingDetails)
+                .HasForeignKey(e => e.AnimalId)
+                .OnDelete(DeleteBehavior.Restrict);
+        });
     }
 }
--- Application.Web/Program.cs
+++ Application.Web/Program.cs
@@ -1,7 +1,13 @@
+using Application.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MainContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
