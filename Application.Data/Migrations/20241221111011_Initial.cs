673c575dfd83c3ea3d851b9b1f668bad38d57623
Markiesch <marksmeel@gmail.com>
2024-12-21 20:40:45 +0100

    Validatie verplaatst naar models

--- Application.Data/Migrations/20241221111011_Initial.cs
+++ Application.Data/Migrations/20241221111011_Initial.cs
@@ -20,7 +20,7 @@ namespace Application.Data.Migrations
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
-                    Type = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false),
                     Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
                     Image = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
                 },
@@ -54,16 +54,16 @@ namespace Application.Data.Migrations
                     Infix = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: true),
                     LastName = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
                     Address = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
-                    CustomerCardType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerCardType = table.Column<int>(type: "int", nullable: true),
                     UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
-                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     NormalizedEmail = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
                     EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
                     PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
                     PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
                     TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
                     LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
@@ -187,17 +187,17 @@ namespace Application.Data.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Bevestigd = table.Column<bool>(type: "bit", nullable: false),
-                    Totaalprijs = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
-                    KlantId = table.Column<int>(type: "int", nullable: false)
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Confirmed = table.Column<bool>(type: "bit", nullable: false),
+                    Totalprice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CustomerId = table.Column<int>(type: "int", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Bookings", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_Bookings_AspNetUsers_KlantId",
-                        column: x => x.KlantId,
+                        name: "FK_Bookings_AspNetUsers_CustomerId",
+                        column: x => x.CustomerId,
                         principalTable: "AspNetUsers",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
@@ -220,7 +220,7 @@ namespace Application.Data.Migrations
                         column: x => x.AnimalId,
                         principalTable: "Animals",
                         principalColumn: "Id",
-                        onDelete: ReferentialAction.Restrict);
+                        onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
                         name: "FK_BookingDetails_Bookings_BookingId",
                         column: x => x.BookingId,
@@ -234,22 +234,22 @@ namespace Application.Data.Migrations
                 columns: new[] { "Id", "Image", "Name", "Price", "Type" },
                 values: new object[,]
                 {
-                    { 1, "/images/aap.jpg", "Aap", 25.0m, "Jungle" },
-                    { 2, "/images/olifant.jpg", "Olifant", 100.0m, "Jungle" },
-                    { 3, "/images/zebra.jpg", "Zebra", 50.0m, "Jungle" },
-                    { 4, "/images/leeuw.jpg", "Leeuw", 150.0m, "Jungle" },
-                    { 5, "/images/hond.jpg", "Hond", 20.0m, "Farm" },
-                    { 6, "/images/ezel.jpg", "Ezel", 30.0m, "Farm" },
-                    { 7, "/images/koe.jpg", "Koe", 50.0m, "Farm" },
-                    { 8, "/images/eend.jpg", "Eend", 15.0m, "Farm" },
-                    { 9, "/images/kuiken.jpg", "Kuiken", 10.0m, "Farm" },
-                    { 10, "/images/pinguin.jpg", "Pinguïn", 80.0m, "Snow" },
-                    { 11, "/images/ijsbeer.jpg", "IJsbeer", 200.0m, "Snow" },
-                    { 12, "/images/zeehond.jpg", "Zeehond", 60.0m, "Snow" },
-                    { 13, "/images/kameel.jpg", "Kameel", 70.0m, "Desert" },
-                    { 14, "/images/slang.jpg", "Slang", 40.0m, "Desert" },
-                    { 15, "/images/trex.jpg", "T-Rex", 500.0m, "Vip" },
-                    { 16, "/images/unicorn.jpg", "Unicorn", 1000.0m, "Vip" }
+                    { 1, "/img/animals/monkey.png", "Aap", 25.0m, 1 },
+                    { 2, "/img/animals/elephant.png", "Olifant", 100.0m, 1 },
+                    { 3, "/img/animals/zebra.png", "Zebra", 50.0m, 1 },
+                    { 4, "/img/animals/lion.png", "Leeuw", 150.0m, 1 },
+                    { 5, "/img/animals/dog.png", "Hond", 20.0m, 0 },
+                    { 6, "/img/animals/donkey.png", "Ezel", 30.0m, 0 },
+                    { 7, "/img/animals/cow.png", "Koe", 50.0m, 0 },
+                    { 8, "/img/animals/duck.png", "Eend", 15.0m, 0 },
+                    { 9, "/img/animals/chicken.png", "Kuiken", 10.0m, 0 },
+                    { 10, "/img/animals/penguin.png", "Pinguïn", 80.0m, 2 },
+                    { 11, "/img/animals/polar-bear.png", "IJsbeer", 200.0m, 2 },
+                    { 12, "/img/animals/seal.png", "Zeehond", 60.0m, 2 },
+                    { 13, "/img/animals/camel.png", "Kameel", 70.0m, 3 },
+                    { 14, "/img/animals/snake.png", "Slang", 40.0m, 3 },
+                    { 15, "/img/animals/t-rex.png", "T-Rex", 500.0m, 4 },
+                    { 16, "/img/animals/unicorn.png", "Unicorn", 1000.0m, 4 }
                 });
 
             migrationBuilder.CreateIndex(
@@ -302,9 +302,9 @@ namespace Application.Data.Migrations
                 column: "BookingId");
 
             migrationBuilder.CreateIndex(
-                name: "IX_Bookings_KlantId",
+                name: "IX_Bookings_CustomerId",
                 table: "Bookings",
-                column: "KlantId");
+                column: "CustomerId");
         }
 
         /// <inheritdoc />
