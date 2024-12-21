b63b8d9a5a41101dca0814395e883ed16b9c9002
Markiesch <marksmeel@gmail.com>
2024-12-21 20:41:20 +0100

    Image verplicht gemaakt

--- Application.Data/Dto/UpdateAnimalDto.cs
+++ Application.Data/Dto/UpdateAnimalDto.cs
@@ -7,5 +7,5 @@ public class UpdateAnimalDto
     public required string Name { get; init; }
     public required AnimalType Type { get; init; }
     public required decimal Price { get; init; }
-    public required string? Image { get; init; }
+    public required string Image { get; init; }
 }
