f335ed6b97fc97448ee8aea00048f787cf02ac39
Markiesch <marksmeel@gmail.com>
2024-12-21 20:40:51 +0100

    Validatie regels toegevoegd

--- Application.Web/Models/AnimalUpdateViewModel.cs
+++ Application.Web/Models/AnimalUpdateViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Application.Data.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+using Application.Data.Dto;
 using Application.Data.Models;
 
 namespace Application.Web.Models;
@@ -6,10 +7,22 @@ namespace Application.Web.Models;
 public class AnimalUpdateViewModel
 {
     public required int Id { get; set; }
+
+    [Required(ErrorMessage = "Naam is verplicht")]
+    [MaxLength(100, ErrorMessage = "Naam mag maximaal 100 karakters bevatten")]
     public required string Name { get; set; }
+
+    [Required(ErrorMessage = "Type is verplicht")]
+    [EnumDataType(typeof(AnimalType), ErrorMessage = "Type is ongeldig")]
     public required AnimalType Type { get; set; }
+    
+    [Required(ErrorMessage = "Prijs is verplicht")]
+    [DataType(DataType.Currency)]
     public required decimal Price { get; set; }
-    public required string? Image { get; set; }
+    
+    [Required(ErrorMessage = "Afbeelding is verplicht")]
+    [StringLength(200, ErrorMessage = "Afbeelding mag maximaal 200 karakters bevatten")]
+    public required string Image { get; set; }
     
     public UpdateAnimalDto ToDto()
     {
