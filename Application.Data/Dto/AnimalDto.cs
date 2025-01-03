3fbb4770f27a4a5868607e93890cc824e2b31038
Markiesch <marksmeel@gmail.com>
2025-01-03 14:49:25 +0100

    #7 fix optional warning

--- Application.Data/Dto/AnimalDto.cs
+++ Application.Data/Dto/AnimalDto.cs
@@ -8,6 +8,6 @@ public class AnimalDto
     public required string Name { get; init; }
     public required AnimalType Type { get; init; }
     public required decimal Price { get; init; }
-    public required string? Image { get; init; }
+    public required string Image { get; init; }
     public required bool IsAvailable { get; init; }
 }
