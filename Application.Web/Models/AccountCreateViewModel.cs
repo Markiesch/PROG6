ffebc1c370daea8988d18be4035471ec2ba7a8c4
Markiesch <marksmeel@gmail.com>
2025-01-22 14:29:18 +0100

    #8 Error messages verbeterd

--- Application.Web/Models/AccountCreateViewModel.cs
+++ Application.Web/Models/AccountCreateViewModel.cs
@@ -1,14 +1,20 @@
-﻿using Application.Data.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Application.Data.Models;
 
 namespace Application.Web.Models;
 
 public class AccountCreateViewModel
 {
+    [Required(ErrorMessage = "Voornaam is verplicht")]
     public string FirstName { get; set; }
     public string? Infix { get; set; }
+    [Required(ErrorMessage = "Achternaam is verplicht")]
     public string LastName { get; set; }
+    [Required(ErrorMessage = "Adres is verplicht")]
     public string Address { get; set; }
-    public string PhoneNumber { get; set; }
+    [Required(ErrorMessage = "Email is verplicht")]
     public string Email { get; set; }
+    [Required(ErrorMessage = "Telefoonnummer is verplicht")]
+    public string PhoneNumber { get; set; }
     public CustomerCardType? CustomerCardType { get; set; }
 }
